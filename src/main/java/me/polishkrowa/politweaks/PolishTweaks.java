9fc3359201a0984f83b69cd1cc51c0d08e77286c
Mrredstone5230 <45701824+Mrredstone5230@users.noreply.github.com>
2022-06-09 15:07:21 -0400

    Initial commit

---
+++ src/main/java/me/polishkrowa/politweaks/PolishTweaks.java
@@ -0,0 +1,15 @@
+package me.polishkrowa.politweaks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PolishTweaks implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
