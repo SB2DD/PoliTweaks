13ee40fd120408da7fff762e15508b28302fb35a
Mrredstone5230 <45701824+Mrredstone5230@users.noreply.github.com>
2022-06-22 12:05:28 -0400

    Adding "seecret.txt" from VanillaCord. (For BungeeGuard support)

--- src/main/java/me/polishkrowa/politweaks/mixin/ServerHandshakeNetworkHandlerMixin.java
+++ src/main/java/me/polishkrowa/politweaks/mixin/ServerHandshakeNetworkHandlerMixin.java
@@ -7,13 +7,20 @@ import me.polishkrowa.politweaks.whatever.BungeeClientConnection;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkState;
 import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
+import net.minecraft.network.packet.s2c.login.LoginDisconnectS2CPacket;
 import net.minecraft.server.network.ServerHandshakeNetworkHandler;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Mixin(ServerHandshakeNetworkHandler.class)
 public class ServerHandshakeNetworkHandlerMixin {
@@ -24,19 +31,78 @@ public class ServerHandshakeNetworkHandlerMixin {
     @Final
     private ClientConnection connection;
 
+    private static String[] seecret = null;
+
     @Inject(method = "onHandshake", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerLoginNetworkHandler;<init>(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/network/ClientConnection;)V"))
-    private void injected(HandshakeC2SPacket packet, CallbackInfo ci) {
+    private void injected(HandshakeC2SPacket packet, CallbackInfo ci) throws IOException {
         if (packet.getIntendedState().equals(NetworkState.LOGIN)) {
             String[] split = ((HandshakeC2SPacketAccessor) packet).getAddress().split("\00");
             if (split.length == 3 || split.length == 4) {
                 ((ClientConnectionAccessor) connection).setAddress(new java.net.InetSocketAddress(split[1], ((java.net.InetSocketAddress) connection.getAddress()).getPort()));
                 ((BungeeClientConnection) connection).setSpoofedUUID(UUIDTypeAdapter.fromString(split[2]));
 
-                if (split.length == 4) {
+                if (getSeecret().length == 0) {
                     ((BungeeClientConnection) connection).setSpoofedProfile(gson.fromJson(split[3], Property[].class));
+                    return;
+                } else if (split.length == 4) {
+                    Property[] properties = gson.fromJson(split[3], Property[].class);
+
+                    Property[] modified = new Property[properties.length - 1];
+
+                    int i = 0;
+                    boolean found = false;
+                    for (Property property : properties) {
+                        if ("bungeeguard-token".equals(property.getName())) {
+                            if (!(found = !found && Arrays.binarySearch(seecret, property.getValue()) >= 0)) {
+                                break;
+                            }
+                        } else if (i != modified.length) {
+                            modified[i++] = property;
+                        }
+                    }
+                    if (found) {
+                        ((BungeeClientConnection) connection).setSpoofedProfile(modified);
+                    } else {
+                        Text disconnectMessage = Text.literal("Invalid forwarding info. Please contact Admin.");
+                        connection.send(new LoginDisconnectS2CPacket(disconnectMessage));
+                        connection.disconnect(disconnectMessage);
+                    }
+
+                } else {
+                    Text disconnectMessage = Text.literal("If you wish to use IP forwarding, please enable it in your BungeeCord config as well!");
+                    connection.send(new LoginDisconnectS2CPacket(disconnectMessage));
+                    connection.disconnect(disconnectMessage);
                 }
             }
         }
+    }
+
+    private static String[] getSeecret() throws IOException {
+        if (seecret == null) {
+            File config = new File("seecret.txt");
+            if (config.exists()) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(config)))) {
+                    HashSet<String> tokens = new HashSet<>();
+                    for (String line; (line = reader.readLine()) != null;) {
+                        if (line.startsWith("bungeeguard-token=") && (line = line.substring(18)).length() != 0) {
+                            tokens.add(line);
+                        }
+                    }
+                    Arrays.sort(seecret = (tokens.size() != 0)? tokens.toArray(new String[0]) : new String[0]);
+                }
+            } else {
+                seecret = new String[0];
+                PrintWriter writer = new PrintWriter(config, StandardCharsets.UTF_8.name());
+                writer.println("# As you may know, standard IP forwarding procedure is to accept connections from any and all proxies.");
+                writer.println("# If you'd like to change that, you can do so by entering BungeeGuard tokens here.");
+                writer.println("# ");
+                writer.println("# This file is automatically generated by VanillaCord once a player attempts to join the server.");
+                writer.println();
+                writer.println("bungeeguard-token=");
+                writer.close();
+            }
+        }
+        return seecret;
     }
 
 }
