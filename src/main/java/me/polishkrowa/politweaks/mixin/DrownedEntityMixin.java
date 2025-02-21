d5b8762194b5232cdfce88b22507b66ee5246a22
SB2DD <45701824+Mrredstone5230@users.noreply.github.com>
2025-02-21 16:25:21 -0500

    update to 1.21.4. Fixed tridents and disabled instant tadpoles

--- src/main/java/me/polishkrowa/politweaks/mixin/DrownedEntityMixin.java
+++ src/main/java/me/polishkrowa/politweaks/mixin/DrownedEntityMixin.java
@@ -1,23 +1,18 @@
 package me.polishkrowa.politweaks.mixin;
 
-import net.minecraft.block.CauldronBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DrownedEntity.class)
 public class DrownedEntityMixin extends MobEntity {
@@ -58,7 +53,4 @@ public class DrownedEntityMixin extends MobEntity {
     //
     //       }
 
-    public void equipStack(EquipmentSlot slot, ItemStack stack) {
-
-    }
 }
